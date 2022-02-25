31822173e867699c5f1e9bc4f658bd98e2d2e1db
fededav <67575679+raishid@users.noreply.github.com>
2022-02-25 17:31:20 -0400

    upp
    ya se cual podria ser el problema intenta desitanalr la dev dependencia y ponerla como dependencias.

--- resource/js/app.js
+++ resource/js/app.js
@@ -1,14 +1,17 @@
 import Vue from "vue";
 import VueSweetalert2 from 'vue-sweetalert2';
 
+Vue.config.productionTip = false
 
 window.axios = require('axios');
 window.Vue = require("vue");
 
 Vue.use(VueSweetalert2);
 
-Vue.component('login-form', require('./components/login/Login.vue').default);
+/* Vue.component('login-form', require('./components/login/Login.vue').default);
 Vue.component('u-table', require('./components/users/TableUsers.vue').default);
-Vue.component('u-modal', require('./components/users/ModalUser.vue').default);
+Vue.component('u-modal', require('./components/users/ModalUser.vue').default); */
 
-const app = new Vue().$mount('#app');
+const app = new Vue({
+    el: '#app'
+})
